6f0b0b13e2b727990335b39158e8f250ba05e2a3
Leonardo A. Reichert <leoreichert5000@gmail.com>
2022-10-23 00:54:09 -0300

    Agregado un nuevo archivo .py para probar push

---
+++ testing.py
@@ -0,0 +1,14 @@
+
+from tkinter import *;
+
+root = Tk();
+root.geometry("200x150");
+root.title("Testing Git");
+
+Label(root, text="This is a test of git! :D").pack();
+
+
+Button(root, text="exit", command=root.destroy).pack(side="bottom");
+
+root.mainloop();
+
