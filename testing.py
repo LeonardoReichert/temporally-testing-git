f46a66c533fee93c71a88bac6a4413d70ede3323
Leonardo A. Reichert <leoreichert5000@gmail.com>
2022-10-30 16:56:20 -0300

    commit para probar idle

--- testing.py
+++ testing.py
@@ -6,7 +6,7 @@ root.geometry("200x150");
 root.title("Testing Git");
 
 Label(root, text="This is a test of git! :D").pack();
-
+Label(root, text="other test of git! :D").pack();
 
 Button(root, text="exit", command=root.destroy).pack(side="bottom");
 
